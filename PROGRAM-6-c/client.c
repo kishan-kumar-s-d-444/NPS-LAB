3a04c69f1e285537d163d5be9aa4137c625727b4
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-06-28 21:49:18 +0530

    6-c

---
+++ PROGRAM-6-c/client.c
@@ -0,0 +1,39 @@
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<arpa/inet.h>
+#include<string.h>
+
+void str_cli(FILE *fp, int sockfd, struct sockaddr *serv_address, int servlen) {
+    int bufsize = 1024, cont;
+    char *buffer = malloc(bufsize);
+    struct sockaddr_in addr;
+
+    while (fgets(buffer, bufsize, fp) != NULL) {
+        sendto(sockfd, buffer, bufsize, 0, serv_address, servlen);
+        cont = recvfrom(sockfd, buffer, bufsize, 0, (struct sockaddr*)&addr,sizeof(addr));
+        if (cont > 0) {
+            printf("Reply from %s: ", inet_ntoa(addr.sin_addr));
+            fputs(buffer, stdout);
+        }
+    }
+    printf("\nEOF\n");
+    free(buffer);
+}
+
+int main(int argc, char *argv[]) {
+    int sockfd;
+    struct sockaddr_in addr;
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(16001);
+    inet_pton(AF_INET, argv[1], &addr.sin_addr);
+
+    str_cli(stdin, sockfd, (struct sockaddr*)&addr, sizeof(addr));
+    close(sockfd);
+    return 0;
+}
+
---
+++ PROGRAM-6-c/server.c
@@ -0,0 +1,40 @@
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<arpa/inet.h>
+#include<string.h>
+
+void str_echo(int sockfd) {
+    int bufsize = 1024, n;
+    char *buffer = malloc(bufsize);
+    struct sockaddr_in cli_address;
+    socklen_t addrlen = sizeof(cli_address);
+
+    while (1) {
+        n = recvfrom(sockfd, buffer, bufsize, 0, (struct sockaddr*)&cli_address, &addrlen);
+        sendto(sockfd, buffer, n, 0, (struct sockaddr*)&cli_address, addrlen);
+    }
+    free(buffer);
+}
+
+int main() {
+    int sockfd;
+    struct sockaddr_in addr;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    printf("The socket was created\n");
+
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(16001);
+
+    if (bind(sockfd, (struct sockaddr*)&serv_address, sizeof(serv_address)) == 0) printf("Binding Socket\n");
+
+    str_echo(sockfd);
+    close(sockfd);
+    return 0;
+}
+
